c76dbcb9f17bb5a588be699e1c845d1ed2c65d98
Pre1ude0 <larss.jakobsons@icloud.com>
2024-10-02 22:18:08 +0300

    Finished :3

--- 3_stunda/main.py
+++ 3_stunda/main.py
@@ -1,6 +1,7 @@
 import math
 
-prices = input("").split(" ")
+prices = input("> ").split(" ")
+print("\n")
 prices = [int(x) for x in prices]
 
 prices.sort(reverse=True)
@@ -10,18 +11,14 @@ finalSum = 0
 triples = math.floor(itemAmount / 3)
 splitPrices = []
 
-
-def sort_index(lst, rev=True):
-    index = range(len(lst))
-    s = sorted(index, reverse=rev, key=lambda i: lst[i])
-    return s
-
-
 for i in range(triples):
-    l = prices[:3]
-    splitPrices.append(l)
-    for p in l:
-        prices.remove(p)
+    maxp = prices[:2]
+    minp = prices[-1]
+
+    splitPrices.append([maxp[0], maxp[1], minp])
+    prices.remove(maxp[0])
+    prices.remove(maxp[1])
+    prices.remove(minp)
 
 print(splitPrices)
 print(prices)
@@ -33,4 +30,5 @@ for t in splitPrices:
 for i in prices:
     finalSum += i
 
+print("\n")
 print(f"Final sum for all items: {finalSum}")
